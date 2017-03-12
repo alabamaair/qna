745998f88948d9046af27aa680465ef7a8c44082
Alabama Air <alabama.air@gmail.com>
2017-03-12 03:16:22 +0300

    answers spec

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,23 @@
+class AnswersController < ApplicationController
+
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+    @question = Question.find params[:question_id]
+    @answer = @question.answers.build(answer_params)
+
+    if @answer.save
+      redirect_to @question, notice: 'Thanks for your answer!'
+    else
+      redirect_to @question, alert: 'Answer can\'t be blank!'
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+end
---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  let!(:question) { create :question }
+
+  describe 'POST #create' do
+    subject { post :create, params: { answer: attributes_for(:answer), question_id: question } }
+
+    context 'with valid attributes' do
+      it 'create answer in database' do
+        expect { subject }.to change(question.answers, :count).by(1)
+      end
+    end
+
+    context 'with invalid attributes' do
+      subject { post :create, params: { answer: attributes_for(:invalid_answer), question_id: question } }
+
+      it 'not create answer in DB' do
+        expect { subject }.to_not change(question.answers, :count)
+      end
+    end
+  end
+
+
+end
