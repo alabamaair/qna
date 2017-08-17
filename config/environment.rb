164856ba3cc5e443d8fc5aac6d11a59179222d5a
Alabama Air <alabama.air@gmail.com>
2017-08-17 14:24:08 +0300

    Smtp

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.yandex.ru',
+  port: 25,
+  user_name: ENV['MAILER_EMAIL'],
+  password: ENV['MAILER_PASSWORD'],
+  authentication: :plain,
+  enable_starttls_auto: true,
+  openssl_verify_mode: 'none'
+}
